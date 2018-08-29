2a0b98a3e82b76c43062bb807fcf4696c3e2c103
gigabites <8arcena@gmail.com>
2018-08-29 15:01:02 -0700

    Implement pull invoices to index.html.

--- app/mod_api/__init__.py
+++ app/mod_api/__init__.py
@@ -1,15 +1,34 @@
 from flask import Blueprint, request
 from flask_restful import Resource, Api, marshal_with, fields, reqparse
 
-from ..models import db, RecurringInvoice
+from ..models import db, RecurringInvoice, Invoice
 from datetime import datetime
 api_module = Blueprint('api',__name__,url_prefix='/api')
 api = Api(api_module)
 
+class CustomDateField(fields.Raw):
+	def format(self, value):
+		return value.strftime("%m/%d/%Y") if type(value) == datetime and datetime else ""
+
+class DollarAmountField(fields.Raw):
+	def format(self, value):
+		return "${0:,.2f}".format(float(value)/100) if value and value > 0 else '$0.00'
+
 recurring_invoice_fields = {
 	'client_name' : fields.String,
 	'service_name' : fields.String,
-	'start_date' : fields.DateTime(dt_format='iso8601'),
+	'start_date' : CustomDateField,
+	'amount' : DollarAmountField,
+}
+
+invoice_fields = {
+	'customer_name' : fields.String,
+	'number' : fields.String,
+	'amount_due' : DollarAmountField,
+	'amount_paid' : DollarAmountField,
+	'void' : fields.String,
+	'generated_filename' : fields.String,
+	'date_due' : CustomDateField,
 }
 
 parser = reqparse.RequestParser()
@@ -29,8 +48,6 @@ class RecurringInvoiceResource(Resource):
 	@marshal_with(recurring_invoice_fields)
 	def post(self):
 		args = parser.parse_args()
-		print(args)
-		print(request.json)
 		new_recurring_invoice = RecurringInvoice(
 			client_name = args['client_name'],
 			amount = args['amount'],
@@ -42,6 +59,14 @@ class RecurringInvoiceResource(Resource):
 		db.session.commit()
 		return new_recurring_invoice
 
-		
+
+class InvoiceResource(Resource):
+	@marshal_with(invoice_fields)
+	def get(self):
+		invoices = Invoice.query.order_by(Invoice.date_due.desc()).all()
+		return invoices
+	
+
 
 api.add_resource(RecurringInvoiceResource, '/recurring-invoices')
+api.add_resource(InvoiceResource, '/invoices')
